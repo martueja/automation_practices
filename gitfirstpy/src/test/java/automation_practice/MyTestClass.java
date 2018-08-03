c5fd5d46859db1c02a5ab4c865d5ea6cb277f0d6
Martha Sandoval <msandoval@H7798.ad.hugeinc.com>
2018-08-03 13:51:03 -0500

    Commiting git project files

---
+++ gitfirstpy/src/test/java/automation_practice/MyTestClass.java
@@ -0,0 +1,16 @@
+package automation_practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyTestClass {
+
+    @Test
+    public void shouldAnswerWithTrue()
+
+    {
+        assertTrue( true );
+        System.out.println("Fin de la prueba");
+    }
+}
